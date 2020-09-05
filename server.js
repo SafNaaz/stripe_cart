7053059d855afcdc4397e1bbcbe36840d582de00
Safnas <iamsafnas@gmail.com>
2020-09-05 21:57:32 +0530

    changed store.html to ejs and rendered items.json

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+
+app.get('/store', (req,res)=>{
+    fs.readFile('items.json',(err, data)=>{
+        if(err){
+            res.status(500).end()
+        }else{
+            res.render('store.ejs', {
+                items : JSON.parse(data)
+            })
+        }
+    })
+})
 
 app.listen(3000)
