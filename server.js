26689004d49b9555c059c208dcda1699a26e3d56
Safnas <iamsafnas@gmail.com>
2020-09-05 23:44:23 +0530

    calling stripe api to complete payment

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
 const express = require('express')
 const app = express()
 const fs = require('fs')
+const stripe = require('stripe')(stripeSecretKey)
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/store', (req,res)=>{
@@ -30,7 +32,40 @@ app.post('/purchase', (req,res)=>{
         if(err){
             res.status(500).end()
         }else{
-            console.log('purchase')
+            const itemsJson = JSON.parse(data)
+            const itemsArray = itemsJson.music.concat(itemsJson.merch)
+            let total = 0
+            req.body.items.forEach(item => {
+                const itemJson = itemsArray.find( i =>{
+                    return i.id == item.id
+                })
+                total = total + itemJson.price * item.quantity
+            });
+
+            stripe.charges.create({
+                amount : total,
+                source : req.body.stripeTokenId,
+                currency : 'usd',
+                //added below for bypassing indian restrictions
+                description: 'Software development services',
+                shipping: {
+                    name: 'Jenny Rosen',
+                    address: {
+                      line1: '510 Townsend St',
+                      postal_code: '98140',
+                      city: 'San Francisco',
+                      state: 'CA',
+                      country: 'US',
+                    },
+                  }
+            }).then(()=>{
+                console.log('Charge Successful')
+                res.json({message:"Successfully purchased items"})
+            }).catch((err)=>{
+                console.log(err)
+                console.log('Charge Fail')
+                res.status(500).end()
+            })
         }
     })
 })
